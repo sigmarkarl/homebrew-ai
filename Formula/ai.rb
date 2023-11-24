f4824c600c5bc88da6bc36f93e83ef10fe61d3d3
Sigmar Stefansson <sigmarkarl@hotmail.com>
2023-11-24 10:09:26 +0000

    add formula

---
+++ Formula/ai.rb
@@ -0,0 +1,18 @@
+class OfasWebsocketTranscoder < Formula
+    desc "Convenience wrapper for gh copilot module"
+    homepage "https://github.com/sigmarkarl/homebrew-ai"
+    url "https://github.com/sigmarkarl/homebrew-ai/releases/download/1.0.0/ai"
+    sha256 "fe607d93b673de995d590da5a5171e119eccf6e373fe68639179cbf4769be3a9"
+    license "MIT License"
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      bin.install "#{buildpath}/ai"
+      system "chmod", "+x", "#{bin}/ai"
+    end
+  
+    test do
+      system "false"
+    end
+  end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Sigmarkarl ai for terminal
+
+## How do I install these formulae?
+
+`brew install sigmarkarl/ai/<formula>`
+
+Or `brew tap sigmarkarl/ai` and then `brew install ai`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
