c321ef13f2f6b9b443e6dff3ce3a39c852d880d5
Sigmar Stefansson <sigmarkarl@hotmail.com>
2023-11-26 22:04:04 +0000

    add x64

--- Formula/ai.rb
+++ Formula/ai.rb
@@ -1,9 +1,14 @@
 class Ai < Formula
     desc "Convenience wrapper for gh copilot module"
     homepage "https://github.com/sigmarkarl/homebrew-ai"
-    url "https://github.com/sigmarkarl/homebrew-ai/releases/download/1.0/ai"
-    sha256 "62373ea1bee3b22b0eee558a5087cc09e80de22b2d4cbd31dda9befef25928b5"
     license "MIT License"
+    if Hardware::CPU.arm?
+      url "https://github.com/sigmarkarl/homebrew-ai/releases/download/1.0/ai"
+      sha256 "62373ea1bee3b22b0eee558a5087cc09e80de22b2d4cbd31dda9befef25928b5"
+    else
+      url "https://github.com/sigmarkarl/homebrew-ai/releases/download/1.0x/ai"
+      sha256 "62373ea1bee3b22b0eee558a5087cc09e80de22b2d4cbd31dda9befef25928b5"
+    end
   
     # depends_on "cmake" => :build
   
